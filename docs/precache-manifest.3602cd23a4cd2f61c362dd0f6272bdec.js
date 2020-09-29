798987e25f98e9ac878191e3e5deee8a9f70ed9c
Joshua Smith <jsmith@stash.energy>
2020-09-29 09:55:37 -0300

    Updates

---
+++ docs/precache-manifest.3602cd23a4cd2f61c362dd0f6272bdec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e89fcfb67348d00140691d129cea4d93",
+    "url": "/react-native-msal/index.html"
+  },
+  {
+    "revision": "971faf6f60f9b463fcb4",
+    "url": "/react-native-msal/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "49f1454e6477926e666e",
+    "url": "/react-native-msal/static/js/2.c6355af8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-native-msal/static/js/2.c6355af8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "971faf6f60f9b463fcb4",
+    "url": "/react-native-msal/static/js/main.e52e953c.chunk.js"
+  },
+  {
+    "revision": "124e280c0fb3b1346b6b",
+    "url": "/react-native-msal/static/js/runtime-main.acce7d27.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-native-msal/static/media/logo.5d5d9eef.svg"
+  }
+]);
